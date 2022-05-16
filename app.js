2d8e682b8a9422e2ad3a7f599da23b89ba454e53
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-05-16 18:50:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,67 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+const app = express();
+
+const helpers = require('./helpers');
+
+app.use(express.static(__dirname + '/public'));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+app.post('/upload-profile-pic', (req, res) => {
+
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
+
+
+    upload(req, res, function(err) {
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        console.log(req.body.userName1);
+        
+        
+    });
 });
+
+app.post('/upload-multiple-images', (req, res) => {
+
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).array('multiple_images', 10);
+
+    upload(req, res, function(err) {
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+
+        console.log(req.body);
+
+        
+    });
+});
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+  });
+
+  app.listen(8080);
