a11e930b467925dcdaffc33ff1562554ce9ce9e4
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2022-05-17 09:54:58 +0300

    Delete helpers.js

--- helpers.js
+++
@@ -1,9 +0,0 @@
-const imageFilter = function(req, file, cb) {
-    // Принимать только изображения
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Разрешены только файлы изображений!';
-        return cb(new Error('Разрешены только файлы изображений!'), false);
-    }
-    cb(null, true);
-};
-exports.imageFilter = imageFilter;
